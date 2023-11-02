d909689d675f53cfc2be8a303d2cf55900dd9066
devnyks <tunahanozturkiletisim@gmail.com>
2023-11-02 12:55:05 +0300

    Updated course navigation with added left and right arrow buttons and a random course generation feature.

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ import Loading from "./components/Loading";
 function App() {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
-  const deleteCourse = (id) => {
-    const afterDeletedCourse = courses.filter((course) => course.id !== id);
-    setCourses(afterDeletedCourse);
-  };
   const fetchCourses = async () => {
     setLoading(true);
     try {
@@ -37,16 +33,7 @@ function App() {
             <div>
               <h1>My Courses</h1>
             </div>
-            {courses.length === 0 ? (
-              <div>
-                <h2>You deleted all courses!</h2>
-                <button style={{ marginLeft: "20px" }} onClick={fetchCourses}>
-                  Refresh
-                </button>
-              </div>
-            ) : (
-              <Courses courses={courses} removeCourse={deleteCourse} />
-            )}
+            <Courses courses={courses} />
           </>
         )}
       </div>
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,13 +1,64 @@
-import Course from "./Course";
+import { useState } from "react";
+import {
+  HiOutlineArrowSmallRight,
+  HiOutlineArrowSmallLeft,
+} from "react-icons/hi2";
 
-function Courses({ courses, removeCourse }) {
+function Courses({ courses }) {
+  const coursesLength = courses.length - 1;
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
+  const randomCourseGenerate = () => {
+    setIndex(Math.floor(Math.random() * courses.length));
+  };
+
+  const prevCourse = () => {
+    setIndex(index === 0 ? coursesLength : index - 1);
+  };
+
+  const nextCourse = () => {
+    setIndex(index === coursesLength ? 0 : index + 1);
+  };
+
   return (
     <>
-      {courses.map((course) => {
+      <div className="button-container">
+        <button onClick={randomCourseGenerate}>Generate Random Course</button>
+      </div>
+      <div className="container">
+        <div className="icon-left">
+          <HiOutlineArrowSmallLeft
+            style={{
+              fontSize: "32px",
+              padding: "10px",
+              margin: "5px",
+              borderRadius: "50%",
+            }}
+            onClick={prevCourse}
+          />
+        </div>
+        <div className="courseBox">
+          <h2>{title}</h2>
+          <p>{content}</p>
+          <h4>PRICE: {price} TL</h4>
+        </div>
+        <div className="icon-right" onClick={nextCourse}>
+          <HiOutlineArrowSmallRight
+            style={{
+              fontSize: "32px",
+              padding: "10px",
+              margin: "5px",
+              borderRadius: "50%",
+            }}
+          />
+        </div>
+        {/* {courses.map((course) => {
         return (
           <Course key={course.id} {...course} removeOneCourse={removeCourse} />
         );
-      })}
+      })} */}
+      </div>
     </>
   );
 }
